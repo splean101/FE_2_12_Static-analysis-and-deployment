2a6af73d4c93d2f88fa60c797975a5481c5395ae
splean101 <101splean@gmail.com>
2023-03-21 18:40:00 +0200

    4

--- src/components/LogOut.js
+++ src/components/LogOut.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 function LogOut() {
   const dispatchFunc = useDispatch();
   const isLoggedIn = useSelector((state) => state.isLoggedIn);
   const logOut = () => {
-    dispatchFunc({ type: 'logOut' })
-  }
+    dispatchFunc({ type: 'logOut' });
+  };
   return (
-    isLoggedIn && <button className='logout' onClick={logOut}>Log Out</button>
-  )
+    isLoggedIn && (
+      <button className="logout" onClick={logOut}>
+        Log Out
+      </button>
+    )
+  );
 }
 
-export default LogOut
+export default LogOut;
--- src/components/Products.js
+++ src/components/Products.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Products = () => {
-
   const coffee = useSelector((state) => state.coffee);
   const sugar = useSelector((state) => state.sugar);
   const isLoggedIn = useSelector((state) => state.isLoggedIn);
@@ -10,41 +9,41 @@ const Products = () => {
   const dispatchFunc = useDispatch();
 
   const incrementCoffee = () => {
-    dispatchFunc({ type: 'incrementCoffee' })
-  }
+    dispatchFunc({ type: 'incrementCoffee' });
+  };
   const decrementCoffee = () => {
-    dispatchFunc({ type: 'decrementCoffee' })
-  }
+    dispatchFunc({ type: 'decrementCoffee' });
+  };
   const increaseCoffee = () => {
-    dispatchFunc({ type: 'increaseCoffee', number: 10 })
-  }
+    dispatchFunc({ type: 'increaseCoffee', number: 10 });
+  };
 
   const incrementSugar = () => {
-    dispatchFunc({ type: 'incrementSugar' })
-  }
+    dispatchFunc({ type: 'incrementSugar' });
+  };
   const decrementSugar = () => {
-    dispatchFunc({ type: 'decrementSugar' })
-  }
+    dispatchFunc({ type: 'decrementSugar' });
+  };
   const increaseSugar = () => {
-    dispatchFunc({ type: 'increaseSugar', number: 10 })
-  }
+    dispatchFunc({ type: 'increaseSugar', number: 10 });
+  };
 
   return (
     <>
-      <div className='product'>
+      <div className="product">
         <span>{`Coffee: ${coffee}`}</span>
         <button onClick={incrementCoffee}>+</button>
         <button onClick={decrementCoffee}>-</button>
         {isLoggedIn && <button onClick={increaseCoffee}>+10</button>}
       </div>
-      <div className='product'>
+      <div className="product">
         <span>{`Sugar: ${sugar}`}</span>
         <button onClick={incrementSugar}>+</button>
         <button onClick={decrementSugar}>-</button>
         {isLoggedIn && <button onClick={increaseSugar}>+10</button>}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
